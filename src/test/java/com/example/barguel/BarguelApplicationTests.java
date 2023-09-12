36ef191faf71eceb289170df0cac16548c9b5cd0
Ingridnery <neryingrid16@gmail.com>
2023-09-12 09:25:26 -0300

    initial commit

---
+++ src/test/java/com/example/barguel/BarguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarguelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
