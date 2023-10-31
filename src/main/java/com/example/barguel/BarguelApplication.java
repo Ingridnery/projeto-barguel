7aac4ae4775bb88695123d19b2c5994b94ac9b34
Ingridnery <neryingrid16@gmail.com>
2023-10-31 08:29:20 -0300

    feat: insert annotations in BarguelApplication

--- src/main/java/com/example/barguel/BarguelApplication.java
+++ src/main/java/com/example/barguel/BarguelApplication.java
@@ -2,8 +2,12 @@ package com.example.barguel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Controller
 public class BarguelApplication {
 
     public static void main(String[] args) {
